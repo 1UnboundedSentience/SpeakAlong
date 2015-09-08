c023f0880b7e89775a144d82933602f02431f5b0
Jason Xu & wildraj <pair+1UnboundedSentience.wildraj@devbootcamp.com>
2015-09-08 12:24:50 -0700

    created route for signin

---
+++ orastory/app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:first_name,:last_name,:username,:password,:email))
+    @user.save
+    redirect_to user_path
+  end
+end
