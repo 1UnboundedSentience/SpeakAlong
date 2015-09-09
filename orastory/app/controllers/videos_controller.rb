f49a4a1f003b5100eaad7600bc718942bd331340
Jason Xu & wildraj <pair+1UnboundedSentience.wildraj@devbootcamp.com>
2015-09-09 12:32:04 -0700

    working on videos

--- orastory/app/controllers/videos_controller.rb
+++ orastory/app/controllers/videos_controller.rb
@@ -3,26 +3,33 @@ class VideosController < ApplicationController
   def index
     @videos = Video.all
     # VideoInfo.provider_api_keys = { youtube: 'YOUR_YOUTUBE_API_KEY', vimeo: 'YOUR_VIMEO_API_KEY' }
-
     @user = User.find(session[:user_id])
-  end
-
-  def show
-
   end
 
   def create
     @user = User.find(params[:user_id])
     p @user
+    @videos = Video.all
     @video = @user.videos.new(
       title: params[:title],
+      user_id: params[:user_id],
       video_link: params[:video_link]
     )
     p @video
     if @video.save
-      redirect_to user_video_path(@user)
+      redirect_to user_video_path(@user, @video)
     else
       render 'index'
     end
   end
+
+  def show
+    @video = Video.find(params[:id]) #the video id
+    @user = User.find(session[:user_id])
+  end
+
+  def edit
+
+  end
+
 end
--- orastory/db/seeds.rb
+++ orastory/db/seeds.rb
@@ -10,8 +10,6 @@ User.create(first_name: "Jason", last_name: "Xu", username: "jasonxu", email: "a
 User.create(first_name: "Topher", last_name: "Lubaway", email: "a@a.com", username: "tl", password: "a")
 User.create(first_name: "Rajal", last_name: "Patel", email: "rj@a.com", username: "rj", password: "a")
 
-1.upto(4) {|n|
-  Video.create(title: "Sherif Talking", user_id: n, video_link: 'https://www.youtube.com/watch?v=8ONf8SXbGZ4')
-}
+  Video.create(title: "Sherif Talking", user_id: 1, video_link: 'https://www.youtube.com/watch?v=8ONf8SXbGZ4')
 
-Video.create(title: "Cat Video", user_id: 1, video_link: 'www.youtube.com/watch?v=tntOCGkgt98')
+Video.create(title: "Cat Video", user_id: 2, video_link: 'www.youtube.com/watch?v=tntOCGkgt98')
