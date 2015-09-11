9108ac23c54b8d23b9572f1c9bc473716e143d10
1UnboundedSentience <jsnx21@gmail.com>
2015-09-11 08:21:09 -0700

    tidy up videos show

--- orastory/app/controllers/reviews_controller.rb
+++ orastory/app/controllers/reviews_controller.rb
@@ -12,6 +12,11 @@ class ReviewsController < ApplicationController
     redirect_to user_video_path(params[:user_id], params[:video_id])
   end
 
+  def update
+    Review.update(review_params)
+    redirect_to user_video_path(params[:user_id], params[:video_id])
+  end
+
   private
     def review_params
       params.require(:review).permit(:body_lang_rating, :body_lang_comments, :vocal_rating, :vocal_comments, :content_rating, :content_comments, :overall_rating, :overall_comments)
