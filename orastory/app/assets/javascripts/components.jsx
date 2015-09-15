da1bbb784e9a0ff040a2228bfd9b0457ff5796ed
Jason Xu & Erick Gist <pair+1UnboundedSentience.lee-egist@devbootcamp.com>
2015-09-15 16:09:39 -0700

    progress 4:09pm sept15

--- orastory/app/assets/javascripts/components.jsx
+++ orastory/app/assets/javascripts/components.jsx
@@ -1,11 +1,4 @@
 //= require_tree ./components
-
-var VIDEOS = [
-        {title: 'Schlieffen',
-        videoID: '4jNjJueYnLI'},
-        {title: 'Simulation Hypothesis',
-        videoID: 'dqVrIBkhqOo'}
-      ]
       var SearchBar = React.createClass({
         wumpus: function() {
           this.props.onUserInput(
@@ -47,6 +40,7 @@ var VIDEOS = [
           console.log(this.props)
           var filterText = this.props.filterText;
           this.props.videos.forEach(function(video) {
+              debugger
               if (video.title.toLowerCase().indexOf(this.props.filterText.toLowerCase()) === -1) { return;}
               rows.push(<VideoRow video={video} key={video.name} />);
           }.bind(this));
@@ -82,8 +76,8 @@ var VIDEOS = [
                     <SearchBar
                       filterText={this.state.filterText}
                       onUserInput={this.handleUserInput} />
-                    <VideoTable videos={this.state.videos}
-
+                    <VideoTable
+                      videos={this.state.videos}
                       filterText={this.state.filterText} />
                 </div>
             );
@@ -96,104 +90,3 @@ var VIDEOS = [
       });
     React.render(<FilterableVideoTable />, document.footer);
 
-// //= require_tree ./components
-// //console.log($('div[data-react-class="Videos"]'))
-// var VIDEOS = [
-//         {title: 'Best Ted Talks',
-//         videoID: '4vl6wCiUZYc'},
-//         {title: 'Addiction every you about addiction is wrong',
-//         videoID: 'PY9DcIMGxMs'}
-//       ]
-// var SearchBar = React.createClass({
-//   wumpus: function() {
-//     this.props.onUserInput(
-//       this.refs.filterTextInput.getDOMNode().value
-//     )
-//   },
-//   render: function() {
-//       return (
-//           <form>
-//             <input
-//               type="text"
-//               placeholder="Search..."
-//               ref = "filterTextInput" //refering to filterTextInput
-//               onChange = {this.wumpus} />
-//           </form>
-//       );
-//   }
-// });
-
-// var VideoRow = React.createClass({
-//   render: function() {
-//       console.log(this.props)
-//       console.log(this.props.video.video_link)
-//       ytLink = this.props.video.video_link
-//       return (
-//           <tr>
-//               <td>{this.props.video.title}</td>
-//               <td><iframe width="560" height="315" src={'http://www.youtube.com/embed/j6cxZp4ii6c?autoplay=true'} frameBorder="0" allowFullScreen></iframe></td>
-//           </tr>
-//           );
-//       }
-//   });
-
-
-
-// var VideoTable = React.createClass({
-//   render: function() {
-//     var rows = [];
-//     console.log(this.props)
-//     console.log(this.props.videos)
-//     var filterText = this.props.filterText;
-//     // this.props.videos.forEach(function(video) {
-//     //     if (video.title.toLowerCase().indexOf(this.props.filterText.toLowerCase()) === -1) { return;}
-//     //     rows.push(<VideoRow video={video} key={video.name} />);
-//     // }.bind(this));
-//     for(var key in this.props.videos) {
-//         console.log(key)
-//         if (this.props.videos[key].title.toLowerCase().indexOf(this.props.filterText.toLowerCase()) === -1) { return;}
-//         rows.push(<VideoRow video={this.props.videos[key]} />);
-//     }
-//     return (
-//         <table>
-//             <tbody>{rows}</tbody>
-//         </table>
-//     );
-//   }
-// });
-
-// var FilterableVideoTable = React.createClass({
-//   handleUserInput: function(wumpus) {
-//     this.setState({
-//       filterText: wumpus
-//     })
-//   },
-//   getInitialState: function() {
-//     console.log(this.props)
-//     return {filterText: '',
-//             videos: this.props
-//           }
-//   },
-//   render: function() {
-//       console.log('videos file render')
-//       console.log(this.state)
-//       // console.log(this.props)
-//       console.log(this.props)
-//       return (
-//           <div>
-//               <SearchBar
-//                 filterText={this.state.filterText}
-//                 onUserInput={this.handleUserInput} />
-//               <VideoTable videos={this.state.videos}
-
-//                 filterText={this.state.filterText} />
-//           </div>
-//       );
-//   },
-//   componentDidMount: function() {
-//     $.get(this.props.source, function(result) {
-
-//     })
-//   }
-// });
-// React.render(<FilterableVideoTable />, document.footer);
