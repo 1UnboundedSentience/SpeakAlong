0f4f3dce47014a84f83a23e824ba855a75b19b4c
1UnboundedSentience <jsnx21@gmail.com>
2015-11-06 17:06:41 -0800

    pushing to heroku

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Orastory
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
-
+end
 # module Speakalong
 #   class Application < Rails::Application
 #     #config.assets.initialize_on_precompile = false
