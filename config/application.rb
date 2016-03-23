44e2d2e94213130c78a9e4e7a0a151d777c7d19f
1UnboundedSentience <jsnx21@gmail.com>
2016-03-22 23:51:58 -0700

    deployment

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Orastory
+module SpeakAlong
   class Application < Rails::Application
     #config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
